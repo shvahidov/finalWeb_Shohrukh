2d48ee82b2e2293d1c1f9db8d0e88c930a3615e8
shvahidov <147513886+shvahidov@users.noreply.github.com>
2023-10-10 19:03:50 +0500

    меню, заголовок и фото добавлини

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        myFioletoviy: '#9065B8',
+        myTemnoFioletoviy: '#5E4278',
+        mySvetloFioletoviy: '#AE7ADE',
+        myDarkfFoletoviy: '#2C1F38',
+        myVerySvetloFioletoviy: '#C288F7'
+      }
+    },
   },
   plugins: [],
+
 }
