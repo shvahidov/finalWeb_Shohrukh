98661e7fd57ecf7a2b978f475a4a72fee17b0669
shvahidov <147513886+shvahidov@users.noreply.github.com>
2023-10-18 19:00:36 +0500

    В разработке контента
    надо переместит последний элемент в середину и поработатаь над текстом и визуальностью

---
+++ src/scriptvideo.js
@@ -0,0 +1,16 @@
+let video = document.getElementById("my-video")
+let playButton  = document.getElementById("play-button")
+let controls = 0;
+function watching(){
+    if(controls == 0){
+        video.play();
+        controls = 1;
+        playButton.style.opacity = "0.1";
+    }
+    else if(controls = 1){
+        video.pause();
+        controls = 0;
+        playButton.style.opacity = "1";
+    }
+}
+playButton.addEventListener("click", watching)
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         brightRed: 'hsl(12, 88%, 59%)', 
         brightLight: 'hsl(12, 88%, 69%)', 
         brightRedLight: 'hsl(12, 88%, 79%)', 
-        brightRedSupLight: 'hsl(267, 45%, 62%)', 
+        brightRedSupLight: 'hsl(12, 88%, 95%)', 
         darkBlue: 'hsl(228, 39%, 23%)', 
         darkGrayishBlue: 'hsl(228, 12%, 61%)',
         veryDarkBlue: 'hsl(228, 12%, 13%)',
